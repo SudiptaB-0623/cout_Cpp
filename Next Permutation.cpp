2a31ea422e91434868767026965bf7b7eacb74dd
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2022-12-30 13:04:02 +0530

    Create Next Permutation.cpp

---
+++ Next Permutation.cpp
@@ -0,0 +1,10 @@
+//SOlved on Coding Ninjas
+
+#include <bits/stdc++.h> 
+vector<int> nextPermutation(vector<int> &permutation, int n)
+{
+    //  Write your code here.
+    bool a =next_permutation(permutation.begin(),permutation.end());
+    
+    return permutation;
+}
