a323f9601707095058f1ddaecdc9040eaf448d1a
SUDIPTA BASAK <inbox.sudipta@gmail.com>
2023-01-02 09:54:27 +0530

    Create Insertion_Sort.cpp

---
+++ Sorting Algorithms/Insertion_Sort.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;              
+    cout<<"Enter size of array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"\nEnter the elements: ";
+    for(int i=0;i<n;i++)                        //Input Elements
+    {
+        cin>>arr[i];
+    }
+
+    for(int i=1;i<n;i++)                        // Insertion Sort
+    {
+        for(int j=i-1,k=i;j>=0;j--,k--)
+        {
+            if(arr[j]>arr[k])
+                swap(arr[j],arr[k]);
+            else
+                break;
+        }
+    }
+
+    for(int i=0;i<n;i++)                        //Output Elements
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
